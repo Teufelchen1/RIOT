ff7c8bb4e8c4e9a5d76a472f1bcaac8640f99d5e
Teufelchen1 <bennet.blischke@haw-hamburg.de>
2023-08-10 13:24:28 +0200

    setup

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,10 +20,38 @@
  */
 
 #include <stdio.h>
+#include "net/gcoap/forward_proxy.h"
+#include "net/gcoap.h"
+#include "net/sock/udp.h"
+#include "net/sock/util.h"
+
+const unsigned char payload[] = {
+    'c', 'o', 'a', 'p', ':', '/', '/', '[',':',':','1',']',':',
+    'P', 'A', 'Y', 'L', 'O', 'A', 'D', '!', 0x06, 0x48, 0x88, 0x93,
+    '/', '\0'
+};
 
 int main(void)
 {
     puts("Hello World!");
+
+    unsigned char buf[512];
+    sock_udp_ep_t client;
+    coap_pkt_t pkt;
+    if(sock_udp_str2ep(&client, "[::1]:1234") != 0) {
+        puts("Failed to setup socket");
+        return -1;
+    }
+    int res = gcoap_req_init(&pkt, buf, sizeof(buf), 0, "/");
+    if (res < 0) {
+        puts("Failed to init packet");
+        return -1;
+    }
+    if(coap_opt_add_proxy_uri(&pkt, (const char *) payload) < 0) {
+        puts("Failed to add proxy uri");
+        return -1;
+    }
+    gcoap_forward_proxy_request_process(&pkt, &client);
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
